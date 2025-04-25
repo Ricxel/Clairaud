7ccca9553c9b46da490f733606ee3a512326946d
Ricxel <giovannisbalchiero04ita@gmail.com>
2025-04-25 13:07:30 +0200

    Tolta funzione getTracks

--- app/src/main/java/com/omasba/clairaud/autoeq/utils/AutoEqualizerUtils.kt
+++ app/src/main/java/com/omasba/clairaud/autoeq/utils/AutoEqualizerUtils.kt
@@ -27,47 +27,5 @@ class AutoEqualizerUtils {
             val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
             context.startActivity(intent)
         }
-
-        fun getTrackTags(artist: String, track: String, apiKey: String): Set<Tag> {
-            val client = OkHttpClient()
-            //metto il mio deserializzatore personalizzato per avere subito i tag
-            val gson = GsonBuilder()
-                .registerTypeAdapter(TrackTagsResponse::class.java, TrackTagsResponseDeserializer())
-                .create()
-
-
-            val encodedArtist = URLEncoder.encode(artist, "UTF-8")
-            val encodedTrack = URLEncoder.encode(track, "UTF-8")
-
-            val url = "https://ws.audioscrobbler.com/2.0/?" +
-                    "method=track.gettoptags" +
-                    "&artist=$encodedArtist" +
-                    "&track=$encodedTrack" +
-                    "&api_key=$apiKey" +
-                    "&format=json"
-
-            val request = Request.Builder()
-                .url(url)
-                .build()
-
-            client.newCall(request).execute().use { response ->
-                if (!response.isSuccessful) throw Exception("HTTP error: ${response.code}")
-
-                val body = response.body?.string() ?: return emptySet()
-
-                val json = gson.fromJson(body, TrackTagsResponse::class.java)
-
-                val tagSet =
-                    json.tags //ottengo i tag direttamente in un set<String> perchè sto usando il sezializzatore custom
-                Log.d("TrackTags", "$tagSet")
-
-                var tagSetRet = mutableSetOf<Tag>()
-                tagSet.forEach{
-                    tagSetRet.add(Tag(name = it))
-                }
-                return tagSetRet
-            }
-        }
     }
-
 }
