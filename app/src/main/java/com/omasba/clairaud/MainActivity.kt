935c65b54bca52d51721a76d2d95688f483f2d8b
Ricxel <giovannisbalchiero04ita@gmail.com>
2025-07-20 15:28:33 +0200

    Separazione fra servizio e broadcast receiver

--- app/src/main/java/com/omasba/clairaud/MainActivity.kt
+++ app/src/main/java/com/omasba/clairaud/MainActivity.kt
@@ -2,6 +2,7 @@ package com.omasba.clairaud
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
